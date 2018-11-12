875073643009ada97e39d52bd802d33e7825ec6a
chanstal <43198566+chanstal@users.noreply.github.com>
2018-11-12 14:42:37 +0800

    Update 輸入整數輸出三角形.java

--- 輸入整數輸出三角形.java
+++ 輸入整數輸出三角形.java
@@ -6,7 +6,7 @@ public class Main {
         Scanner scn=new Scanner(System.in);
         int n=scn.nextInt();
       int m=1;
-        for(int i=0;i<n;i++) {
+        for(int i=1;i<=n;i++) {
             for (int j = 0; j < n - i - 1; j++) {
                 System.out.print(" ");
             }
