6a02e38482088e3db36d38e57787d96f221cf083
chanstal <43198566+chanstal@users.noreply.github.com>
2018-11-12 14:43:34 +0800

    Update and rename 輸入整數和字串 輸出到三角形.java to 輸入整數和字串 輸出倒三角形.java

--- 輸入整數和字串 輸出倒三角形.java
+++ 輸入整數和字串 輸出倒三角形.java
@@ -6,7 +6,7 @@ public class Main {
         Scanner scn=new Scanner(System.in);
         int n=scn.nextInt();
         String str=scn.next();
-        for(int i=0;i<n;i++){
+        for(int i=1;i<=n;i++){
             for(int j=0;j<n-i;j++){
                 System.out.print(str);
             }
